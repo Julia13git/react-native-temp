daa802282f6564c7ab18a2e1a89335dfe793400a
Yuliia Hots <goculiya@gmail.com>
2024-07-11 18:19:26 +0200

    ajout les composants input avec le style

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { Text, View } from "react-native";
+import { ImageBackground, Text, View } from "react-native";
 import { s } from "./App.style";
+import hotBackground from "./assets/hot.png";
+import { InputTemperature } from "./components/InputTemperature";
 
 export default function App() {
   return (
-    <View style={s.container}>
+    <ImageBackground source={hotBackground} style={s.container}>
       <View style={s.workspace}>
         <View>
           <Text>Temperature</Text>
         </View>
-        <View>
-          <Text>Input</Text>
-        </View>
+        <InputTemperature defaultValue={"12"} />
         <View>
           <Text>Button</Text>
         </View>
       </View>
-    </View>
+    </ImageBackground>
   );
 }
--- App.style.js
+++ App.style.js
@@ -1,7 +1,7 @@
 import { StyleSheet } from "react-native";
 
 const s = StyleSheet.create({
-  container: { flex: 1, justifyContent: "center" },
+  container: { flex: 1, justifyContent: "center", padding: 20 },
   workspace: {
     height: 450,
     alignItems: "center",
