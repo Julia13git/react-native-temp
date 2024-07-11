a20062444e011d24fac784c82769a0a9f7a7951c
Yuliia Hots <goculiya@gmail.com>
2024-07-11 17:33:20 +0200

    Ajout les images

--- App.js
+++ App.js
@@ -1,8 +1,20 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
+import { s } from "./App.style";
 
 export default function App() {
-  return <Text> Hello!
-  </Text>
+  return (
+    <View style={s.container}>
+      <View style={s.workspace}>
+        <View>
+          <Text>Temperature</Text>
+        </View>
+        <View>
+          <Text>Input</Text>
+        </View>
+        <View>
+          <Text>Button</Text>
+        </View>
+      </View>
+    </View>
+  );
 }
-
-
