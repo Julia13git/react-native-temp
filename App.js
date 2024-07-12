25572ec3196ecf20595a230a82ab0c4387f5252c
Yuliia Hots <goculiya@gmail.com>
2024-07-12 12:51:44 +0200

    Le changement du background

--- App.js
+++ App.js
@@ -1,18 +1,23 @@
 import { ImageBackground, Text, View } from "react-native";
 import { s } from "./App.style";
 import hotBackground from "./assets/hot.png";
+import coldBackground from "./assets/cold.png";
 import { InputTemperature } from "./components/InputTemperature/InputTemperature";
 import { TemperatureDisplay } from "./components/TemperatureDisplay/TemperatureDisplay";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { DEFAULT_TEMPERATURE, DEFAULT_UNIT, UNITS } from "./constant.js";
 import {
   getOppositUnit,
   convertTemperatureTo,
+  isIceTemperature,
 } from "./services/temperature-service.js";
+import { ButtonConvert } from "./components/ButtonConvert/ButtonConvert.jsx";
 
 export default function App() {
   const [inputValue, setInputValue] = useState(DEFAULT_TEMPERATURE);
   const [currentUnit, setCurrentUnit] = useState(DEFAULT_UNIT);
+  const [currentBackground, setCurrentBackground] = useState();
+
   const oppositeUnit = getOppositUnit(currentUnit);
 
   function getConvertedTemperature() {
@@ -22,19 +27,33 @@ export default function App() {
       : convertTemperatureTo(oppositeUnit, valueAsFloat).toFixed(1);
   }
 
+  useEffect(() => {
+    const temperatureAsFloat = Number.parseFloat(inputValue);
+    if (!isNaN(temperatureAsFloat)) {
+      const isColdBackground = isIceTemperature(inputValue, currentUnit);
+      setCurrentBackground(isColdBackground ? coldBackground : hotBackground);
+    }
+  }, [inputValue, currentUnit]);
+
   return (
-    <ImageBackground source={hotBackground} style={s.container}>
+    <ImageBackground source={currentBackground} style={s.container}>
       <View style={s.workspace}>
         <TemperatureDisplay
           value={getConvertedTemperature()}
           unit={oppositeUnit}
         />
         <InputTemperature
+          unit={currentUnit}
           onChangeText={setInputValue}
           defaultValue={DEFAULT_TEMPERATURE}
         />
         <View>
-          <Text>Button</Text>
+          <ButtonConvert
+            onPress={() => {
+              setCurrentUnit(oppositeUnit);
+            }}
+            unit={currentUnit}
+          />
         </View>
       </View>
     </ImageBackground>
