e444605d91cf07a92a10293f417f9e9a8bd4f524
Yuliia Hots <goculiya@gmail.com>
2024-07-12 11:54:47 +0200

    ajout de constant et converteur

--- App.js
+++ App.js
@@ -1,16 +1,38 @@
 import { ImageBackground, Text, View } from "react-native";
 import { s } from "./App.style";
 import hotBackground from "./assets/hot.png";
-import { InputTemperature } from "./components/InputTemperature";
+import { InputTemperature } from "./components/InputTemperature/InputTemperature";
+import { TemperatureDisplay } from "./components/TemperatureDisplay/TemperatureDisplay";
+import { useState } from "react";
+import { DEFAULT_TEMPERATURE, DEFAULT_UNIT, UNITS } from "./constant.js";
+import {
+  getOppositUnit,
+  convertTemperatureTo,
+} from "./services/temperature-service.js";
 
 export default function App() {
+  const [inputValue, setInputValue] = useState(DEFAULT_TEMPERATURE);
+  const [currentUnit, setCurrentUnit] = useState(DEFAULT_UNIT);
+  const oppositeUnit = getOppositUnit(currentUnit);
+
+  function getConvertedTemperature() {
+    const valueAsFloat = Number.parseFloat(inputValue);
+    return isNaN(valueAsFloat)
+      ? ""
+      : convertTemperatureTo(oppositeUnit, valueAsFloat).toFixed(1);
+  }
+
   return (
     <ImageBackground source={hotBackground} style={s.container}>
       <View style={s.workspace}>
-        <View>
-          <Text>Temperature</Text>
-        </View>
-        <InputTemperature defaultValue={"12"} />
+        <TemperatureDisplay
+          value={getConvertedTemperature()}
+          unit={oppositeUnit}
+        />
+        <InputTemperature
+          onChangeText={setInputValue}
+          defaultValue={DEFAULT_TEMPERATURE}
+        />
         <View>
           <Text>Button</Text>
         </View>
